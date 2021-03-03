82418ae02552ed76b22ec9363125ae02a5ed8c7d
DollyChouhan <79242686+DollyChouhan@users.noreply.github.com>
2021-03-03 16:03:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+import PersonList from './Components/PersonList'
+
+function App() {
+  const [people,setPeople] = React.useState(data)
+  return(
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List  people={people}/>
+        <button onClick={()=> setPeople([])}>
+          clear all
+        </button>
+      
+      </section>
+    </main>
+  )
+}
+
+
+export default App;
+
